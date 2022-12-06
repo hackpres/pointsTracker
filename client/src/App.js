2a5b858427c1019ccb554fa175166c70ff02b1bd
hackpres <102390515+hackpres@users.noreply.github.com>
2022-12-06 11:43:57 -0500

    added getUsers function to App.js to fetch userData

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import getUsers from './utils/api';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers().then(
+      json => setUsers(json.userData)
+    ).catch(
+      err => new Error(err)
+    )
+  }, [])
+  console.log(users);
   return (
     <>
-    
+      <h1>Points Tracker</h1>
+      <h2>Select User to view points accrual</h2>
     </>
   );
 }
