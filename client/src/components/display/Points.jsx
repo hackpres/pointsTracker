8e4f0a71c40d16c6690c69ae6166a2b32aa5cef7
hackpres <102390515+hackpres@users.noreply.github.com>
2022-12-06 13:58:35 -0500

    Points component now prints monthly points, purchases and total points for quarter.

--- client/src/components/display/Points.jsx
+++ client/src/components/display/Points.jsx
@@ -12,15 +12,30 @@ function Points(props) {
             return <p>Total points earned = {arr.reduce((accum, curr) => accum + curr)}</p>
         }
     }
+    const getPurchases = (data) => {
+        console.log(data)
+        if (data.length > 0) {
+            let purchases = [];
+            data.forEach(num => purchases.push(parseInt(num)))
+            return data.join(', ')
+        } else return 'No purchases made this month'
+    }
   return (
     <>
         <div>
             {props.quarter.map((month, i) => {
                 let data = Object.entries(month)
                 return (
-                    <p key={i}>
-                        {data[0][0]}: {getPointData(data[0][1])}
-                    </p>
+                    <div key={i}>
+                        <p>
+                            {data[0][0]}: {getPointData(data[0][1])}
+                        </p>
+                        <p>Purchases:</p>
+                        <p>
+                            {getPurchases(data[0][1])}
+                        </p>
+                    </div>
+
                 )
             })}
         </div>
